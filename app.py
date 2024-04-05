368a6389747bf4e35554fefc1e6f4e8d86856dc2
Mihir Shah <89178683+Mojo11@users.noreply.github.com>
2024-04-04 21:42:55 -0400

    Updating app.py
    Adding a part to see the prediction on the page.

--- app.py
+++ app.py
@@ -6,39 +6,28 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load your machine learning model here
 model = load_model('EfficientNet_ModelWeights.keras')
 
-# Prediction function
 def preprocess_and_predict(model, img_path, target_size=(224, 224)):
     # Load and preprocess the image
     img = image.load_img(img_path, target_size=target_size)
-    if img is None:
-        print("Error: Image not loaded.")
-        return None
-    
-    # Converting image to array and preprocessing using EfficientNet's preprocessing
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 
-    # Predicting the class label
-    preds = model.predict(img_array)
-    predicted_label = np.argmax(preds[0])
+    # Make prediction
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction) 
 
-    reverse_expression_labels = {v: k for k, v in expression_labels.items()}
+    # Return the predicted class
+    return predicted_class
 
-    # Converting the predicted label index to its corresponding expression label
-    predicted_expression_label = reverse_expression_labels[predicted_label]
-    
-    return predicted_expression_label
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Assuming you're receiving an image file for prediction
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
@@ -51,8 +40,8 @@ def predict():
     # Make prediction
     predicted_class = preprocess_and_predict(model, file_path)
 
-    # Return the predicted class
-    return jsonify({'prediction': predicted_class})
+    # Return the predicted class as a response
+    return render_template('index.html', prediction=predicted_class)
 
 if __name__ == '__main__':
     app.run(debug=True)
